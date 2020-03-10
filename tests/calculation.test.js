38a0ad66777992368e1aed11b0ec1aebf621c0db
Bart Sztykowski <bartsztykowski@Barts-Air.cable.rcn.com>
2020-03-09 21:18:38 -0400

    test

--- tests/calculation.test.js
+++ tests/calculation.test.js
@@ -6,7 +6,7 @@ const strictEquals = require('../calculation')
 describe('Calculation', () => {
   describe('strictEquals', () => {
     it('returns true if values are same type and same value', () => {
-    
+
       //create test data
       const checkNumberOne = 5
       const checkNumberTwo = 5
@@ -16,40 +16,40 @@ describe('Calculation', () => {
 
       //make assertions about what should be true
       expect(checkResult).to.equal(true)
-      
+
       it('returns false if values are same type and different value', () => {
-                
+
         //create test data
         const checkNumberOne = 5
         const checkNumberTwo = 10
-        
+
         //call the function with the test data
         const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
-        
+
         //make assertions about what should be true
         expect(checkResult).to.equal(false)
 
         it('returns false if values are different type and same value', () => {
-                          
+
           //create test data
           const checkNumberOne = 5
           const checkNumberTwo = '5'
-                
+
           //call the function with the test data
           const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
-                
+
           //make assertions about what should be true
           expect(checkResult).to.equal(false)
 
           it('returns false if values are different type and different value', () => {
-                            
+
             //create test data
             const checkNumberOne = 5
             const checkNumberTwo = '10'
-                        
+
             //call the function with the test data
             const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
-                        
+
             //make assertions about what should be true
             expect(checkResult).to.equal(false)
 
